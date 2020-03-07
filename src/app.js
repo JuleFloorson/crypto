099d427c6df07b03031ab4080bf13ef98f3c584f
JuleFloorson <jubo93@web.de>
2020-03-07 14:17:50 +0100

    corret names camelcase

--- src/app.js
+++ src/app.js
@@ -5,13 +5,13 @@ import companylogo from './picture/logo.png';
 export function app() {
   const header = createElement('header', { className: 'header' });
   const logoImage = createElement('img', {
-    className: 'logoimage',
+    className: 'logoImage',
     src: companylogo
   });
   const button = createElement('btn', { className: 'button' });
-  const stripeOne = createElement('div', { className: 'stripeone' });
-  const stripeTwo = createElement('div', { className: 'stripetwo' });
-  const stripeThree = createElement('div', { className: 'stripethree' });
+  const stripeOne = createElement('div', { className: 'stripeOne' });
+  const stripeTwo = createElement('div', { className: 'stripeTwo' });
+  const stripeThree = createElement('div', { className: 'stripeThree' });
   appendContent(header, [logoImage, button]);
   appendContent(button, [stripeOne, stripeTwo, stripeThree]);
   return [header];
