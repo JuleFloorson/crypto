b8cbf364ba1c3ededda39f839818dc997b31432f
JuleFloorson <jubo93@web.de>
2020-03-07 13:38:21 +0100

    Create header button and style

--- src/app.js
+++ src/app.js
@@ -1,2 +1,18 @@
-// import { createElement } from "./lib/dom";
-// import { appendContent } from "./lib/dom";
+import { createElement, appendContent } from './lib/dom';
+import './app.scss';
+import companylogo from './picture/logo.png';
+
+export function app() {
+  const header = createElement('header', { className: 'header' });
+  const logoImage = createElement('img', {
+    className: 'logoimage',
+    src: companylogo
+  });
+  const button = createElement('btn', { className: 'button' });
+  const stripeOne = createElement('div', { className: 'stripeone' });
+  const stripeTwo = createElement('div', { className: 'stripetwo' });
+  const stripeThree = createElement('div', { className: 'stripethree' });
+  appendContent(header, [logoImage, button]);
+  appendContent(button, [stripeOne, stripeTwo, stripeThree]);
+  return [header];
+}
