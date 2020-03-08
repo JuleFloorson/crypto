bda425453c0ccc9695c808a75644f5c58225b5bf
JuleFloorson <jubo93@web.de>
2020-03-08 16:50:16 +0100

    Add names to test

--- src/app.js
+++ src/app.js
@@ -8,11 +8,25 @@ export function app() {
     className: 'logoImage',
     src: companylogo
   });
+  const main = createElement('main', { className: 'main' });
+
+  const allCoins = ['Bitcoin', 'Etherium', 'Litecoin', 'Dash'];
+
   const button = createElement('btn', { className: 'button' });
   const stripeOne = createElement('div', { className: 'stripeOne' });
   const stripeTwo = createElement('div', { className: 'stripeTwo' });
   const stripeThree = createElement('div', { className: 'stripeThree' });
+
+  allCoins.forEach(coin => {
+    const output = createElement('div', {
+      className: 'output',
+      innerText: coin
+    });
+    appendContent(main, output);
+  });
+
   appendContent(header, [logoImage, button]);
   appendContent(button, [stripeOne, stripeTwo, stripeThree]);
-  return [header];
+
+  return [header, main];
 }
---
+++ src/components/coins.js
@@ -0,0 +1,18 @@
+import './coins.scss';
+// import { createElement, appendContent } from '../lib/dom';
+
+// export function coins(props) {
+//   const container = createElement('div', {
+//     className: 'coins'
+//   });
+//   props.items.forEach(item => {
+//     const element = createElement('div', {
+//       innerText: item,
+//       className: 'coin'
+//     });
+
+//     appendContent(container, element);
+//   });
+//   return container;
+// }
+// const allCoins = ['Bitcoin', 'Etherium', 'Litecoin', 'Dash'];
